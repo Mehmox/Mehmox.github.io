df65259362ceecf43f5c014bd469808fe2a2267e
Mehmox <71236327+Mehmox@users.noreply.github.com>
2024-04-15 21:02:04 +0300

    Delete GAMES directory

--- GAMES/2DGame/js/Coin_algorithm.js
+++
@@ -1,8 +0,0 @@
-function Coincord() {
-    do {
-        CoinY = parseInt(Math.random() * (919 - CoinHeight));
-    } while (PlayerY <= CoinY && CoinY <= PlayerY + PlayerHeight || CoinY <= PlayerY && PlayerY <= CoinY + CoinHeight)
-    do {
-        CoinX = parseInt(Math.random() * (919 - CoinWidth));
-    } while (PlayerX <= CoinX && CoinX <= PlayerX + PlayerWidth || CoinX <= PlayerX && PlayerX <= CoinX + CoinWidth)
-}
